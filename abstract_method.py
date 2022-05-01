0e559c1022180c58ae0d97bb80920fe826d4e38c
Yinon Tzomi <yinontz@edu.hac.ac.il>
2022-05-01 22:09:10 +0300

    Added documentation to abstract method

--- abstract_method.py
+++ abstract_method.py
@@ -1,24 +1,36 @@
 """
 https://docs.python.org/3.5/library/abc.html#abc.abstractmethod
-Here I show how abstractmethod works
+Here I show how abstractmethod works.
 """
 
 from abc import ABC, abstractmethod
 
 
 class AbstractClass(ABC):
-
+    """
+    Abstract class.
+    """
     @abstractmethod
     def abstract_method(self):
+        """
+        Some abstract method.
+        :return:
+        """
         return
 
 
 class ConcreteClass(AbstractClass):
+    """
+    Inherit from AbstractClass and overriding te abstract method.
+    """
     def __init__(self):
         self.me = "me"
 
-    def abstract_method(self):
-        print("ConcreteClass is overloading abstract method")
+    def abstract_method(self) -> None:
+        """
+        Overriding the abstract method.
+        """
+        print("ConcreteClass is overloading abstract method.")
 
 
 class AnotherConcreteClass(AbstractClass):
@@ -27,7 +39,7 @@ class AnotherConcreteClass(AbstractClass):
 
 
 if __name__ == '__main__':
-    # Here you can see that I can create object from ConcreteClass
+    # Here you can see that I can create object from ConcreteClass.
     my_concrete_object = ConcreteClass()
     my_concrete_object.abstract_method()
 
